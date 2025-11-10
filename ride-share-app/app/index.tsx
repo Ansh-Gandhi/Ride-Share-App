91ac2f4e3a5905dc81b80d06cacc2b80bbdedfc1
Ansh Gandhi <anshgandhi@hotmail.com>
2025-11-10 14:59:57 -0800

    completed onboarding pages

---
+++ ride-share-app/app/index.tsx
@@ -0,0 +1,10 @@
+import { Text } from 'react-native';
+import { Redirect } from 'expo-router'
+
+const Home = () => {
+    return (
+        <Redirect href="/(auth)/welcome" />
+    )
+};
+
+export default Home;
